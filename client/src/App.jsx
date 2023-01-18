9ba8642217d0622695f88caaa611ca3c79b0441e
Durand98 <113887080+Durand98@users.noreply.github.com>
2023-01-18 04:55:22 +0000

    Menu 1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,20 +5,19 @@ import './App.css';
 function App() {
   const [count, setCount] = useState('');
 function myFunction(even){
- setCount(even.select)
+  setCount(even.target.value);
+
 }
   function Menu(){
 return (
   <div>
   <h1>Application de gestion de projets</h1>
   <h2>Menu</h2>
-  <span  onChange={myFunction}>
-    <li id="select"><input  onVan="Projet"  type= "checkbox"/>Projet</li>
+  <span onChange={myFunction} >
+    <li id="select"   ><input   type= "checkbox"/>Projet</li>
     <li><input type= "checkbox"/>Equipe</li>
     <li><input type= "checkbox"/>Fichier</li>
     </span>
-
-
   </div>
 );
   }
@@ -29,7 +28,7 @@ return (
       <Menu/>
 
       <p>
-      <strong>{count}</strong>
+      <strong>{count} </strong>
     </p>
 
 </div>
